809f09e812a01833ea4e1afc93a67cec2dc8be78
skhadka90 <shreeya.khadka@gmail.com>
2017-10-24 03:02:26 -0400

    obj det can parse objs

---
+++ utils/create_label_map.py
@@ -0,0 +1,32 @@
+# Shreeya Khadka
+# Create mapping from Index-->String for each input in input protobuf file
+# ==============================================================================
+
+import numpy as np
+
+'''Create label_map from input path to pbtxt'''
+def load_labelmap(path):
+  label_map = {}
+  with open(path) as f:
+    lines = f.readlines()
+    for l in lines:
+      words = l.split(':')
+      words = [w.strip() for w in words] #Get rid of spaces/newlines
+      if words[0]=='id':
+        obj_id = np.int32(words[1])
+      elif words[0]=='display_name':
+        obj_name = words[1]
+        #label_map[obj_id] = {'id':obj_id, 'name':str(obj_name)}
+        label_map[obj_id] = obj_name.strip('\"')
+      else:
+        continue
+  return label_map        
+
+def main():
+  lmap = load_labelmap('../data/mscoco_label_map.pbtxt')    
+  print (lmap)  
+
+#Testing..
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
