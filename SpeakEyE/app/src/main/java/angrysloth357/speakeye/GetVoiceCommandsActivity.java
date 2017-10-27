25dd2325162a95ff8ffaa5362e3d088d05fcc4e2
skhadka90 <shreeya.khadka@gmail.com>
2017-10-27 14:42:19 -0400

    adding android

---
+++ SpeakEyE/app/src/main/java/angrysloth357/speakeye/GetVoiceCommandsActivity.java
@@ -0,0 +1,13 @@
+package angrysloth357.speakeye;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GetVoiceCommandsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_get_voice_commands);
+    }
+}
---
+++ SpeakEyE/app/src/main/java/angrysloth357/speakeye/TextToSpeechActivity.java
@@ -0,0 +1,13 @@
+package angrysloth357.speakeye;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TextToSpeechActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_text_to_speech);
+    }
+}
