c1ee29f1c7b155939836983114caeedd6f6a8253
skhadka90 <shreeya.khadka@gmail.com>
2017-11-08 15:00:33 -0500

    :pig_nose: adding object detection activity

--- SpeakEyE/app/src/main/java/angrysloth357/speakeye/DetectObjectsActivity.java
+++ SpeakEyE/app/src/main/java/angrysloth357/speakeye/DetectObjectsActivity.java
@@ -2,6 +2,15 @@ package angrysloth357.speakeye;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.view.View;
 
 public class DetectObjectsActivity extends AppCompatActivity {
 
@@ -9,7 +18,30 @@ public class DetectObjectsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detect_objects);
+
+        /* Add camera buttons */
+        ImageButton imgBtn = (ImageButton) findViewById(R.id.detectBtn);
+        ImageView mImageView = (ImageView) findViewById(R.id.dispImage);
     }
 
+    /* Shreeya: Add intent to use the Camera */
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    public void dispatchTakePictureIntent(View view) { //View was needed to use the camera :o
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    /*Get image and display it as imageview*/
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        ImageView mImageView = (ImageView) findViewById(R.id.dispImage);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            mImageView.setImageBitmap(imageBitmap);
+        }
+    }
 
 }
