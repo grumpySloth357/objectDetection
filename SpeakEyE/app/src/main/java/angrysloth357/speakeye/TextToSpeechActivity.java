1e06d4b2c767fced3e90efbdd279b847c40db11b
ivyoo00 <31751491+ivyoo00@users.noreply.github.com>
2017-11-08 19:49:48 -0500

    add TextToSpeech

--- SpeakEyE/app/src/main/java/angrysloth357/speakeye/TextToSpeechActivity.java
+++ SpeakEyE/app/src/main/java/angrysloth357/speakeye/TextToSpeechActivity.java
@@ -2,13 +2,76 @@ package angrysloth357.speakeye;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import java.util.Locale;
+
+import android.content.SharedPreferences.Editor;
+import android.speech.tts.TextToSpeech;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
 
 public class TextToSpeechActivity extends AppCompatActivity {
+
+    String text;
+    EditText et;
+    TextToSpeech tts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_text_to_speech);
-        //ajhjajha
+        setContentView(R.layout.activity_main);
+
+        et=(EditText)findViewById(R.id.editText1);
+        tts=new TextToSpeech(TextToSpeechActivity.this, new TextToSpeech.OnInitListener() {
+
+            @Override
+            public void onInit(int status) {
+                // TODO Auto-generated method stub
+                if(status == TextToSpeech.SUCCESS){
+                    int result=tts.setLanguage(Locale.US);
+                    if(result==TextToSpeech.LANG_MISSING_DATA ||
+                            result==TextToSpeech.LANG_NOT_SUPPORTED){
+                        Log.e("error", "This Language is not supported");
+                    }
+                    else{
+                        ConvertTextToSpeech();
+                    }
+                }
+                else
+                    Log.e("error", "Initilization Failed!");
+            }
+        });
+
+
+    }
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+
+        if(tts != null){
+
+            tts.stop();
+            tts.shutdown();
+        }
+        super.onPause();
+    }
+
+    public void onClick(View v){
+
+        ConvertTextToSpeech();
+
+    }
+
+    private void ConvertTextToSpeech() {
+        // TODO Auto-generated method stub
+        text = et.getText().toString();
+        if(text==null||"".equals(text))
+        {
+            text = "Content not available";
+            tts.speak(text, TextToSpeech.QUEUE_FLUSH, null);
+        }else
+            tts.speak(text, TextToSpeech.QUEUE_FLUSH, null);
     }
 }
