51b50721dc002c3d66dd02019a0dbb5c8e0bc7ac
lowycve <wangyc95@bu.edu>
2017-11-10 10:48:16 -0500

    add speech code

--- SpeakEyE/app/src/main/java/angrysloth357/speakeye/MainActivity.java
+++ SpeakEyE/app/src/main/java/angrysloth357/speakeye/MainActivity.java
@@ -4,7 +4,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity
+{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
